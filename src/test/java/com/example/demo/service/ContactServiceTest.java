6b1afcc8320703cd0c25098773eedf974a62ef86
Udai Srinivasa Prasad K S <udai.prasad.5@gmail.com>
2024-07-11 22:30:36 +0530

    Pushed from Automated Test Gen Tool

---
+++ src/test/java/com/example/demo/service/ContactServiceTest.java
@@ -0,0 +1,22 @@
+
+package com.example.demo.controller;
+
+import com.example.demo.model.Contact;
+import com.example.demo.service.ContactService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+import java.util.Optional;
+
+public class ContactServiceTest{
+
+}
